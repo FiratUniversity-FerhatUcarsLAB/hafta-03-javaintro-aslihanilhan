d01f5cdbf3abf804da8a7265234ee2539b0cf355
aslihanilhan <250541098@firat.edu.tr>
2025-10-20 14:00:05 +0300

    Update BesKereTekrar.java

--- Task-1-HosgeldinMesajlari/BesKereTekrar.java
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -1,5 +1,3 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 
 class Main {
     public static void main(String[] args) {
@@ -10,3 +8,4 @@ class Main {
         System.out.println("Java'ya Hoş Geldiniz");
     }
 }
+
