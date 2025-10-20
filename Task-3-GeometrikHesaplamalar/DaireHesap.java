cd834cdf244f27e4ff2cea89d392e2a342fb19e6
aslihanilhan <250541098@firat.edu.tr>
2025-10-20 14:35:19 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,17 @@
+public class DaireHesap {
+    public static void main(String[] args) {
+        double r = 5.5;
+        double pi = 3.14159;
+        // Alan hesapla
+        double alan = pi * r * r;
+        
+        // Çevre hesapla
+        double cevre = 2 * pi * r;
+        
+        // Sonuclari yazdir
+        System.out.println("Yaricap: " + r);
+        System.out.println("Alan: "+ alan);
+        System.out.println("Cevre: " + cevre);
+        
+    }
+}
