29603d15697285e5d3a3d3bb97ba06250fc29a96
aslihanilhan <250541098@firat.edu.tr>
2025-10-20 15:06:07 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,21 @@
+public class İsimKarti {
+    public static void main(String[] args) {
+        String ad = "Aslihan";
+        String soyad = "İlhan";
+        String ogrNo = "250541098";
+        String bolum = "Yazilim Muh.";
+        
+        System.out.println("===================");
+        System.out.println(" OGRENCİ BİLGİLERİ ");
+        System.out.println("Ad    : " + ad);
+        System.out.println("Soyad : " + soyad);
+        System.out.println("No    : " + ogrNo);
+        System.out.println("Bolum : " + bolum);
+        System.out.println("===================");
+        
+        
+        
+        
+        
+    }
+}
