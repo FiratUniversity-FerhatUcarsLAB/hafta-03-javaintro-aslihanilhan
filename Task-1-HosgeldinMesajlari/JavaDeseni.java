ea3083cd1cbf2c4ec2321549446f708487aa4cc9
aslihanilhan <250541098@firat.edu.tr>
2025-10-20 13:46:13 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+ public class JavaDeseni {
+    public static void main(String[] args) {
+        System.out.println("    J A V V A");
+        System.out.println("   J A A V V A A");
+        System.out.println(" J J AAAAA V V AAAAA");
+        System.out.println("   J J A A V A A");
+    }
+}
