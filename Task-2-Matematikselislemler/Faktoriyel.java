b319e833b078e2149359c9c7f6d48e696391aea0
aslihanilhan <250541098@firat.edu.tr>
2025-10-20 14:27:14 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,10 @@
+public class Faktoriyel {
+    public static void main(String[] args) {
+        // Sonucu hesapla
+        int sonuc = 1*2*3*4*5;
+        
+        // Ekrana yazdır
+        System.out.println("1*2*3*4*5 =" + sonuc );
+        
+    }
+}
