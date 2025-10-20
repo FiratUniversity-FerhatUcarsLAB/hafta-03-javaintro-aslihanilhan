67bb79952414752bf18dcb8317ce2dc2ba298200
aslihanilhan <250541098@firat.edu.tr>
2025-10-20 14:43:18 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,17 @@
+public class DikdortgenHesap {
+    public static void main(String[] args) {
+      double en = 4.5;
+      double boy = 7.9;
+      
+      // Alan hesapla
+      double alan = en * boy;
+      
+      // Cevre hesapla
+      double cevre = 2 * en + 2 * boy;
+      
+      //Sonuclari yazdir
+      System.out.println("Alan; " + alan);
+      System.out.println("Cevre; " + cevre);
+        
+    }
+}
