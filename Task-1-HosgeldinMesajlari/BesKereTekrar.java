48a003c04fcabeb6bd5bf871abb0117c77ccbf9c
aslihanilhan <250541098@firat.edu.tr>
2025-10-20 13:50:25 +0300

    Update BesKereTekrar.java

--- Task-1-HosgeldinMesajlari/BesKereTekrar.java
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -3,7 +3,6 @@
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Try programiz.pro");
         System.out.println("Java'ya Hoş Geldiniz");
         System.out.println("Java'ya Hoş Geldiniz");
         System.out.println("Java'ya Hoş Geldiniz");
