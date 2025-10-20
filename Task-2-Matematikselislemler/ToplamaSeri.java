5f0beb3457e5b3bfbb949f5cd590ae15963a5256
aslihanilhan <250541098@firat.edu.tr>
2025-10-20 13:59:11 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,9 @@
+public class ToplamaSeri {
+    public static void main(String[] args) {
+    // Sonucu hesapla
+    int sonuc = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+   
+    // Ekrana yazdır
+     System.out.println(" 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = " + sonuc);
+    }
+}
