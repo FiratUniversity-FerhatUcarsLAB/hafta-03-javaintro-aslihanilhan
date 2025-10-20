2647971d9f6d4fdeec7e5caca28e975145b61823
aslihanilhan <250541098@firat.edu.tr>
2025-10-20 13:34:35 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hoşgeldiniz");
+        System.out.println("Programlama Eğlencelidir");
+    }
+}
