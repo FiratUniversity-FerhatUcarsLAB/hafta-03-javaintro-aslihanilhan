d3b67dfde6f0c1ac2085ab58d055e89674837c29
aslihanilhan <250541098@firat.edu.tr>
2025-10-20 15:22:40 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,12 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+        System.out.println("-----------------------");
+        System.out.println("urun / miktar / fiyat");
+        System.out.println("gomlek / 2 / 250");
+        System.out.println("pantolon / 3 / 400");
+        System.out.println("elbise / 2 / 500");
+        System.out.println("----------------------");
+        
+        
+    }
+}
