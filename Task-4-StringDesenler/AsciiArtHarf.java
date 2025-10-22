a72ca5921337af37c5c7ea383b6f4a765e745333
aslihanilhan <250541098@firat.edu.tr>
2025-10-22 11:58:09 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,13 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+        System.out.println("S   S");
+        System.out.println("SS  S");
+        System.out.println("S S S");
+        
+   
+   
+   
+   
+   
+    }
+}
