1b052f9004918fd7345623844263f973ff8231a5
aslihanilhan <250541098@firat.edu.tr>
2025-10-22 12:25:37 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,31 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+  
+  
+   int mil1 = 1;
+   int km_0 = mil1*1609;
+   
+   int mil5 = 5;
+   int km_1 = mil5*1609;
+   
+   
+   int mil10 = 10;
+   int km_2 = mil10*1609;
+   
+   int mil20 = 20;
+   int km_3 = mil20*1609;
+   
+   
+   int mil50 = 50;
+   int km_4 = mil50*1609;
+   
+   
+   System.out.println("1mil icin km degeri: "+km_0);
+    System.out.println("5mil icin km degeri: "+km_1);
+    System.out.println("10mil icin km degeri: "+km_2);
+    System.out.println("20mil icin km degeri: "+km_3);
+    System.out.println("50mil icin km degeri: "+km_4);
+     
+      }
+
+  }
