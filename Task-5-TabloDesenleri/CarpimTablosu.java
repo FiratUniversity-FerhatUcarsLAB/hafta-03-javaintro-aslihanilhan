819831122a18d2d705394882ffe7745bae687b6b
aslihanilhan <250541098@firat.edu.tr>
2025-10-22 12:10:35 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,11 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+  
+  
+  for(int i=2;i<=2;i++){
+     for(int a=1;a<=10;a++){
+    System.out.println("carpim= " + a+"*"+i+"= "+  a*i);
+      }
+
+  }
+  
