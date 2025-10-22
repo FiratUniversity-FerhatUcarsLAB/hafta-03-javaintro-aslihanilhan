8eca78620488f441f6bd4064c54d0ca296d31fff
aslihanilhan <250541098@firat.edu.tr>
2025-10-22 12:57:07 +0300

    Update UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,72 @@
+
+public class ÜslüSayılarTablosu {
+    public static void main(String[] args) {
+    System.out.print("a");    
+    System.out.print("  ");
+    System.out.print("a^2");
+    System.out.print("  ");
+    System.out.println("a^3");
+ 
+ 
+ 
+ 
+    int a1 = 1;
+    int a2 = 2;
+    int a3 = 3;
+        int a4 = 4;
+    int a5 = 5;
+
+    
+    System.out.print(a1);
+    System.out.print("  ");
+    System.out.print(a1*a1); 
+    System.out.print("     ");
+    System.out.println(a1*a1*a1);
+
+    
+
+    System.out.print(a2);
+    System.out.print("  ");
+    System.out.print(a2*a2); 
+    System.out.print("     ");
+    System.out.println(a2*a2*a2);
+
+    System.out.print(a3);
+    System.out.print("  ");
+    System.out.print(a3*a3); 
+    System.out.print("     ");
+    System.out.println(a3*a3*a3);
+    
+    
+    System.out.print(a4);
+    System.out.print("  ");
+    System.out.print(a4*a4); 
+    System.out.print("    ");
+    System.out.println(a4*a4*a4);
+    
+    
+    System.out.print(a5);
+    System.out.print("  ");
+    System.out.print(a5*a5); 
+    System.out.print("    ");
+    System.out.println(a5*a5*a5);
+
+
+      }
+
+  }
+  
+  
+  
+    
+
+
+      
+
+  
+        
+   
+   
+   
+   
+   
